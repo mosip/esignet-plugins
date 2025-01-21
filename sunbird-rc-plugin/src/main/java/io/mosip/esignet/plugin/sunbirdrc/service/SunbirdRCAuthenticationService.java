763b027f1a05b4bc5eb04b7bd372caafd65a554e
pvsaidurga <saidurgacsea@gmail.com>
2025-01-21 13:11:37 +0530

    [ES-2158] Update rwview comments
    Signed-off-by: pvsaidurga <saidurgacsea@gmail.com>

--- sunbird-rc-plugin/src/main/java/io/mosip/esignet/plugin/sunbirdrc/service/SunbirdRCAuthenticationService.java
+++ sunbird-rc-plugin/src/main/java/io/mosip/esignet/plugin/sunbirdrc/service/SunbirdRCAuthenticationService.java
@@ -121,12 +121,18 @@ public class SunbirdRCAuthenticationService implements Authenticator {
         log.info("Started to build kyc-auth request with transactionId : {} && clientId : {}",
                 kycAuthDto.getTransactionId(), clientId);
         try {
-            for (AuthChallenge authChallenge : kycAuthDto.getChallengeList()) {
-                if(Objects.equals(authChallenge.getAuthFactorType(),"KBI")){
-                    return validateKnowledgeBasedAuth(kycAuthDto.getIndividualId(),authChallenge);
-                }
-            }
-            throw new KycAuthException("invalid_challenge_format");
+            return kycAuthDto.getChallengeList().stream()
+                    .filter(authChallenge -> Objects.equals(authChallenge.getAuthFactorType(), "KBI"))
+                    .findFirst()
+                    .map(authChallenge -> {
+                        try {
+                            return validateKnowledgeBasedAuth(kycAuthDto.getIndividualId(), authChallenge);
+                        } catch (KycAuthException e) {
+                            //When throwing KycAuthException getting "Unhandled exception: io.mosip.esignet.api.exception.KycAuthException is shown", hence throwing RuntimeException
+                            throw new RuntimeException(e);
+                        }
+                    })
+                    .orElseThrow(() -> new KycAuthException("invalid_challenge_format"));
         } catch (KycAuthException e) {
             throw e;
         } catch (Exception e) {
