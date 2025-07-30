ac7b939cb4832ebcb6bdbf1e4df17c90e252bac0
Sachin Rana <sacrana324@gmail.com>
2025-07-30 14:34:58 +0530

    [ES-2396] change in ui-spec response (#137)
    * change in ui-spec response
    
    Signed-off-by: Sachin Rana <sacrana324@gmail.com>
    
    * updated with jsonspec
    
    Signed-off-by: Sachin Rana <sacrana324@gmail.com>
    
    * kept dependency in plugin
    
    Signed-off-by: Sachin Rana <sacrana324@gmail.com>
    
    ---------
    
    Signed-off-by: Sachin Rana <sacrana324@gmail.com>

--- mosip-identity-plugin/src/main/java/io/mosip/signup/plugin/mosipid/service/IdrepoProfileRegistryPluginImpl.java
+++ mosip-identity-plugin/src/main/java/io/mosip/signup/plugin/mosipid/service/IdrepoProfileRegistryPluginImpl.java
@@ -11,6 +11,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ArrayNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.fasterxml.jackson.databind.node.TextNode;
+import com.jayway.jsonpath.JsonPath;
 import io.micrometer.core.annotation.Timed;
 import io.mosip.esignet.core.util.IdentityProviderUtil;
 import io.mosip.signup.plugin.mosipid.dto.VerificationMetadata;
@@ -135,12 +136,29 @@ public class IdrepoProfileRegistryPluginImpl implements ProfileRegistryPlugin {
     @Value("${mosip.signup.mosipid.get-ui-spec.endpoint}")
     private String uiSpecUrl;
 
+    @Value("${mosip.signup.mosipid.uispec.schema-jsonpath:$[0].jsonSpec[0].spec.schema}")
+    private String schemaJsonpath;
+
+    @Value("${mosip.signup.mosipid.uispec.errors-jsonpath:$[0].jsonSpec[0].spec.errors}")
+    private String errorsJsonpath;
+
     private JsonNode uiSpec;
 
     @PostConstruct
     public void init() {
-        this.uiSpec = request(uiSpecUrl, HttpMethod.GET, null, new ParameterizedTypeReference<ResponseWrapper<JsonNode>>() {})
-                .getResponse();
+        String responseJson = request(uiSpecUrl, HttpMethod.GET, null, new ParameterizedTypeReference<ResponseWrapper<JsonNode>>() {
+        })
+                .getResponse()
+                .toString();
+        Object schema = JsonPath.read(responseJson, schemaJsonpath);
+        Object errors = JsonPath.read(responseJson, errorsJsonpath);
+        this.uiSpec = objectMapper.valueToTree(
+                Map.ofEntries(
+                        Map.entry("schema", schema),
+                        Map.entry("errors", errors),
+                        Map.entry("language", Map.of("mandatory", mandatoryLanguages, "optional", optionalLanguages))
+                )
+        );
     }
 
 
