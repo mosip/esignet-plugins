666ce946ff34a7b8b7e10958a0a82f733de7fcc9
Md-Humair-KK <mdhumair.kankudti@gmail.com>
2025-11-19 16:08:34 +0530

    resolve coderabbit comments
    Signed-off-by: Md-Humair-KK <mdhumair.kankudti@gmail.com>

--- sunbird-rc-plugin/src/main/java/io/mosip/esignet/plugin/sunbirdrc/service/SunbirdRCAuthenticationService.java
+++ sunbird-rc-plugin/src/main/java/io/mosip/esignet/plugin/sunbirdrc/service/SunbirdRCAuthenticationService.java
@@ -39,9 +39,9 @@ import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import jakarta.annotation.PostConstruct;
-import javax.validation.Valid;
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import java.nio.charset.StandardCharsets;
 import java.util.*;
 
