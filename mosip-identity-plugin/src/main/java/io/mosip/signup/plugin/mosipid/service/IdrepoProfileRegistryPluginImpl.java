237737a0c531e4c614883eeea1e5cf52d919e2b7
Nandhukumar <nandhukumare@gmail.com>
2026-03-17 17:49:07 +0530

    ES-2955 | fix: idrepo update identity workaround
    Signed-off-by: Nandhukumar <nandhukumare@gmail.com>

--- mosip-identity-plugin/src/main/java/io/mosip/signup/plugin/mosipid/service/IdrepoProfileRegistryPluginImpl.java
+++ mosip-identity-plugin/src/main/java/io/mosip/signup/plugin/mosipid/service/IdrepoProfileRegistryPluginImpl.java
@@ -385,7 +385,7 @@ public class IdrepoProfileRegistryPluginImpl implements ProfileRegistryPlugin {
         if (!StringUtils.isEmpty(responseWrapper.getResponse().getHashValue()) &&
                 !StringUtils.isEmpty(responseWrapper.getResponse().getSalt())) {
             return new Password(responseWrapper.getResponse().getHashValue(),
-                    responseWrapper.getResponse().getSalt());
+                    responseWrapper.getResponse().getSalt(), responseWrapper.getResponse().getHashValue()); // Added for compatible with 1.3.0 IDRepo ('https://github.com/mosip/id-repository/blob/v1.3.0/id-repository/id-repository-identity-service/src/main/java/io/mosip/idrepository/identity/service/impl/IdRepoServiceImpl.java#L508')
         }
         log.error("Failed to generate salted hash {}", responseWrapper.getResponse());
         throw new ProfileException(REQUEST_FAILED);
@@ -484,7 +484,6 @@ public class IdrepoProfileRegistryPluginImpl implements ProfileRegistryPlugin {
         //generate salted hash for password, if exists
         if(inputJson.has("password")) {
             Password password = generateSaltedHash(inputJson.get("password").asText());
-            password.setValue(password.getHashValue()); // Added for compatible with 1.3.0 IDRepo ('https://github.com/mosip/id-repository/blob/v1.3.0/id-repository/id-repository-identity-service/src/main/java/io/mosip/idrepository/identity/service/impl/IdRepoServiceImpl.java#L508')
             ((ObjectNode) inputJson).set("password", objectMapper.valueToTree(password));
         }
 
