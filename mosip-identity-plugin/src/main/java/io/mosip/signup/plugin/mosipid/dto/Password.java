658bc3add8b5f2408cadad27592d88ee4eb5494b
Nandhukumar <nandhukumare@gmail.com>
2026-03-18 10:13:41 +0530

    ES-2955 idrepo update identity workaround (#193)
    * fix: idrepo update identity workaround
    
    Signed-off-by: Nandhukumar <nandhukumare@gmail.com>
    
    * ES-2955 | fix: idrepo update identity workaround
    
    Signed-off-by: Nandhukumar <nandhukumare@gmail.com>
    
    ---------
    
    Signed-off-by: Nandhukumar <nandhukumare@gmail.com>

--- mosip-identity-plugin/src/main/java/io/mosip/signup/plugin/mosipid/dto/Password.java
+++ mosip-identity-plugin/src/main/java/io/mosip/signup/plugin/mosipid/dto/Password.java
@@ -14,6 +14,7 @@ public class Password {
 
     private String hash;
     private String salt;
+    private String value; // Added for compatible with 1.3.0 IDRepo
 
     @Data
     @AllArgsConstructor
